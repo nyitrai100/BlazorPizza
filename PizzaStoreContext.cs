cf5ffecede78f6d3809ca9e9a8f32be33ded3ced
nyitraibence <nyitrai100@gmail.com>
2024-04-11 08:24:23 +0100

    first commit

---
+++ PizzaStoreContext.cs
@@ -0,0 +1,87 @@
+// using Microsoft.EntityFrameworkCore;
+
+// namespace BlazingPizza;
+
+// public class PizzaStoreContext : DbContext
+// {
+//   public PizzaStoreContext(DbContextOptions options) : base(options)
+//   {
+//   }
+
+//   public DbSet<PizzaSpecial> Specials { get; set; }
+// }
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza;
+
+[Route("orders")]
+[ApiController]
+public class OrdersController : Controller
+{
+    private readonly PizzaStoreContext _db;
+
+    public OrdersController(PizzaStoreContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
+    {
+        var orders = await _db.Orders
+ 	    .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+ 	    .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+ 	    .OrderByDescending(o => o.CreatedTime)
+ 	    .ToListAsync();
+
+        return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> PlaceOrder(Order order)
+    {
+        order.CreatedTime = DateTime.Now;
+
+        // Enforce existence of Pizza.SpecialId and Topping.ToppingId
+        // in the database - prevent the submitter from making up
+        // new specials and toppings
+        foreach (var pizza in order.Pizzas)
+        {
+            pizza.SpecialId = pizza.Special.Id;
+            pizza.Special = null;
+        }
+
+        _db.Orders.Attach(order);
+        await _db.SaveChangesAsync();
+
+        return order.OrderId;
+    }
+}
+
+public class PizzaStoreContext : DbContext
+  {
+        public PizzaStoreContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        public DbSet<PizzaSpecial> Specials { get; set; }
+
+        public DbSet<Topping> Toppings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuring a many-to-many special -> topping relationship that is friendly for serialization
+            modelBuilder.Entity<PizzaTopping>().HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+            modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(ps => ps.Toppings);
+            modelBuilder.Entity<PizzaTopping>().HasOne(pst => pst.Topping).WithMany();
+        }
+
+  }
